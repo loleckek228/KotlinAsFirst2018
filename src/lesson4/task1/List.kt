100c5bdc14071e440c6dcc985c1321f7563618e4
loleckek228 <george-valiev@yandex.ru>
2018-10-23 14:04:03 +0300

    исправления четвертого лессона

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -126,13 +126,12 @@ fun abs(v: List<Double>): Double =
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double {
-    return if (list.isEmpty()) {
-        0.0
-    } else {
-        list.sum() / list.size
-    }
-}
+fun mean(list: List<Double>): Double =
+        if (list.isEmpty()) {
+            0.0
+        } else {
+            list.sum() / list.size
+        }
 
 /**
  * Средняя
